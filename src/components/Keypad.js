d8405b0a04bf96494a9e9274283f87b9c0baf45c
Ibrahim Nour <ibrahamnour22@gmail.com>
2021-01-23 20:11:37 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -5,9 +5,7 @@ class Keypad extends React.Component{
   render() {
     return (
       <div>
-    <label for="pass">Password</label>
-    <input type="password" id="pass" name="password"
-           minlength="8" required>
+    <input type="password"/>
 </div>);
   }
 }
